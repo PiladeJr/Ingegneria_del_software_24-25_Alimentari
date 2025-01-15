9f54176c54c495e310ab51ec9216b17353b05372
Federico <fedecor@protonmail.com>
2025-01-15 19:19:05 +0100

    Inizializzazione progetto Spring Boot Gradle Java

---
+++ java/ids_24_25_alimentari/src/main/java/it/cs/unicam/ids_24_25_alimentari/ids_24_25_alimentari/Ids2425AlimentariApplication.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ids2425AlimentariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ids2425AlimentariApplication.class, args);
+	}
+
+}
---
+++ java/ids_24_25_alimentari/src/test/java/it/cs/unicam/ids_24_25_alimentari/ids_24_25_alimentari/Ids2425AlimentariApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ids2425AlimentariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
