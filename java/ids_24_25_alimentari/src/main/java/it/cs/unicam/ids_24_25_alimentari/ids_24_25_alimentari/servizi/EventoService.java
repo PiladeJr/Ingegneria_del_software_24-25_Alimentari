6c83b805c5856207fc92e2eacf09d7a6cd36a0eb
pilade <piladejr.tomassini@studenti.unicam.it>
2025-07-10 01:33:30 +0200

    pulizia di import inutilizzati

--- java/ids_24_25_alimentari/src/main/java/it/cs/unicam/ids_24_25_alimentari/ids_24_25_alimentari/servizi/EventoService.java
+++ java/ids_24_25_alimentari/src/main/java/it/cs/unicam/ids_24_25_alimentari/ids_24_25_alimentari/servizi/EventoService.java
@@ -7,7 +7,6 @@ import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.dto.eventi.VisitaE
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.dto.utente.IscrittoDTO;
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.modelli.azienda.Azienda;
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.modelli.contenuto.eventi.*;
-import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.modelli.eventi.*;
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.modelli.indirizzo.Indirizzo;
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.modelli.utente.Utente;
 import it.cs.unicam.ids_24_25_alimentari.ids_24_25_alimentari.repositories.EventoRepository;
